e00fc5fd59e5a81a873f0dbc67c4a3989f913ff4
Prelang Builder <builder@prelang.com>
2014-07-21 14:25:45 +0000

    Generated scaffolding for model 'Cliente' via Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :clientes
+
   resources :pizzas
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
